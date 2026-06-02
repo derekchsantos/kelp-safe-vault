6fc6de5c68c28552c123e947eb5a4b8dfbbea77a
Derek Christopher Santos <48655103+derekchsantos@users.noreply.github.com>
2026-06-01 22:49:23 -0300

    Annotate DeployScript with author information
    Added author annotation to the DeployScript contract.

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.20;
 import "forge-std/Script.sol";
 import "../contracts/SafeVault.sol";
 
+//@author Derek Christopher
+
 contract DeployScript is Script {
     function run() external {
         // Pega a chave privada do ambiente
